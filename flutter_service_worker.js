598e97939b49d4fe92a6d6bcf2d731833e758158
ennio-dev <ennio.dev@outlook.de>
2026-03-22 00:13:28 +0100

    Add files via upload

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -54,14 +54,14 @@ const RESOURCES = {"assets/AssetManifest.bin": "3f2d946d940a4fba197c7309225f55a0
 "canvaskit/skwasm_heavy.wasm": "b0be7910760d205ea4e011458df6ee01",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "24bc71911b75b5f8135c949e27a2984e",
-"flutter_bootstrap.js": "dda6d545292e37653d28955b48607a0d",
+"flutter_bootstrap.js": "dd4ae910b103d122f892996acf07e492",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
 "index.html": "a0460c18746f68f078b66fb65adac039",
 "/": "a0460c18746f68f078b66fb65adac039",
-"main.dart.js": "44bbec60a0adb6b6bfb464aba1e5c6f5",
+"main.dart.js": "37db969e163031f1a60856cdcb483870",
 "manifest.json": "6d6bc80cb7af153396ebc25f64a7b6d7",
 "version.json": "8c0b33bef9cc819fc29f26fa949fd9af"};
 // The application shell files that are downloaded before a service worker can
