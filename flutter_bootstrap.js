7c078cbaf78c14b407bf8c9e383229f6e7122679
ennio-dev <ennio.dev@outlook.de>
2026-06-28 18:18:24 +0200

    Add files via upload

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"4c525dac5ebe5971c5708ef73558ed8edcf4a3
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2255020723" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "2850145769" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
